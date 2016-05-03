0a15c25f36cbe976068360d0e77e75d9f9869d5f
Kaya M Baber <kaya.baber@gmail.com>
2016-05-02 21:07:19 -0700

    Large Revamp, see description
    -added more comments and empty lines for clarity
    -added win and draw checking functions
    -improved random ai to be more efficient
    -altered game code to allow rand_ai to play against itself
    -altered main code to do basic stats on rand_ai games
    -changed main code to do

--- Tic-Tac-AI/tac_tac_ai.py
+++ Tic-Tac-AI/tac_tac_ai.py
@@ -1,4 +1,5 @@
 import random as rd
+import numpy as np
 
 def display_board(gameBoard):
     numerals="    "
@@ -20,11 +21,14 @@ def display_board(gameBoard):
 def human_player(gameBoard):
     row = raw_input("Row: ")
     col = raw_input("Column: ")
+    
     if (row.isdigit()==False) or (col.isdigit()==False): 
         print "That's not a location. Pick a valid location"
         row, col = human_player(gameBoard)
+        
     row =int(row)-1
     col =int(col)-1
+    
     if (row > len(gameBoard)-1) or (row < 0):
         print "That row isn't on the board. Pick a valid location."
         row, col = human_player(gameBoard)
@@ -36,46 +40,126 @@ def human_player(gameBoard):
         row, col = human_player(gameBoard)
     return row, col
     
+
+def check_win(gameBoard,winLen):
+    #checks if someone won
+    gameBoard=np.array(gameBoard)
+    winStringX="X"*winLen
+    winStringO="O"*winLen
+    
+    for row in gameBoard: #check rows
+        if ("".join(row.tolist())).find(winStringX)==0:
+            return "X"
+        if ("".join(row.tolist())).find(winStringO)==0:
+            return "O"
+            
+    for col in range(len(gameBoard)): #check columns
+        if ("".join(gameBoard[:,col].tolist())).find(winStringX)==0:
+            return "X"
+        if ("".join(gameBoard[:,col].tolist())).find(winStringO)==0:
+            return "O"
+            
+    for d in range(winLen-len(gameBoard),len(gameBoard)-winLen): #check diagonals
+        across=np.diag(gameBoard,d)
+        acrossMirror=np.diag(np.fliplr(gameBoard)) #since numpy diagonals only go one way
+        if ("".join(across.tolist())).find(winStringX)==0:
+            return "X"
+        if ("".join(across.tolist())).find(winStringO)==0:
+            return "O"
+        if ("".join(acrossMirror.tolist())).find(winStringX)==0:
+            return "X"
+        if ("".join(acrossMirror.tolist())).find(winStringO)==0:
+            return "O"
+            
+    return False
+        
+
+def check_full(gameBoard):
+    #checks if the gameboard is full
+    for rows in gameBoard:
+        for cols in rows:
+            if cols ==" ":
+                return False
+    return True
+
+
+def find_openings(gameBoard):
+    openings=[]
+    for rows in range(len(gameBoard)):
+        for cols in range(len(gameBoard)):
+            if gameBoard[rows][cols] ==" ":    
+                openings.append([rows,cols])
+    return openings
+    
     
 def rand_ai(gameBoard):
-    row = rd.randrange(0, len(gameBoard))  
-    col = rd.randrange(0, len(gameBoard))  
-    if gameBoard[row][col] != " ":
-        row,col=0,0        
-        for rows in gameBoard:
-            for cols in rows:
-                if cols ==" ":
-                    row, col = rand_ai(gameBoard)
-                    break
-            break
+    openings=find_openings(gameBoard)
+    choice = rd.randrange(0, len(openings))
+    coord = openings[choice]
+    row=coord[0]
+    col=coord[1]
     return row, col
-
-
-def is_win(gameBoard):
-    winString="X"*len(gameBoard)
-    winString2="O"*len(gameBoard)
-    for row in gameBoard:
-        if row.join == winString1:
-            return "X"
-        if row.join == winString2:
-            return "0"
+    
     
     
 def tic_tac_game(N):
+    
     gameBoard=[]
-    for i in range(N):
+    for i in range(N):                     #initialize game board
         gameBoard.append([" "]*N)
     display_board(gameBoard)
+    
     for i in range((N**2)):
         print "\nPlayer: "+"X"
-        row, col = human_player(gameBoard)
+        #row, col = human_player(gameBoard) #take player move
+        row, col= rand_ai(gameBoard)
         gameBoard[row][col] = "X"
         display_board(gameBoard)
-        row, col= rand_ai(gameBoard)
+        
+        result = check_win(gameBoard,N-1)   #check for player win
+        if result != False:
+            if result == "X":
+                print "X wins!"
+                return "X"
+            if result == "O":
+                print "O wins!"
+                return "O"
+        if check_full(gameBoard):
+            print "Cat's game!"
+            return "Draw"
+        
+        print "\nPlayer: "+"O"
+        row, col= rand_ai(gameBoard)       #take AI move
         gameBoard[row][col] = "O"
         display_board(gameBoard)
-    display_board(gameBoard)
+        
+        result = check_win(gameBoard,N-1)   #check for AI win
+        if result != False:
+            if result == "X":
+                print "X wins!"
+                return "X"
+            if result == "O":
+                print "O wins!"
+                return "O"
+        if check_full(gameBoard):
+            print "Cat's game!"
+            return "Draw"
     return 0
 
-
-tic_tac_game(3)
+winLog=[]
+XCount=0
+OCount=0
+drawCount=0
+for i in range(100):
+    win=tic_tac_game(6)
+    if win == "X":
+        XCount+=1
+    if win== "O":
+        OCount+=1
+    if win=="Draw":
+        drawCount+=1
+    winLog.append(win)
+print winLog
+print "X ", XCount
+print "O ", OCount
+print "Draw ", drawCount
